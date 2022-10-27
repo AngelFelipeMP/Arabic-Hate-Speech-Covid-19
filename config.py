613655456f3fe567fdeca9f7be3c0a3f03c0f6f8
AngelFelipeMP <angel.magnossao@gmail.com>
2022-10-27 16:13:06 +0200

    update

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 import os
 #Hiper-parameters
 SPLITS = 2 #5
-EPOCHS = 2 #5
+EPOCHS = 5 #5
 MAX_LEN = [32] #128
 DROPOUT = [0.3]
 LR = [1e-5]
 BATCH_SIZE = [8] #32
-TRANSFORMERS = ['aubmindlab/bert-base-arabertv02-twitter', 'aubmindlab/araelectra-base-discriminator']
+TRANSFORMERS = ['aubmindlab/araelectra-base-discriminator',
+                'aubmindlab/bert-base-arabertv02-twitter',
+                'xlm-roberta-base',
+                'bert-base-multilingual-cased', 
+                'aubmindlab/aragpt2-base',
+                'asafaya/albert-large-arabic']
 DEVICE = 'cpu'
 
 N_ROWS= 32 #None
@@ -16,19 +21,13 @@ REPO_PATH = '/'.join(CODE_PATH.split('/')[0:-1])
 DATA_PATH = REPO_PATH + '/' + 'data'
 LOGS_PATH = REPO_PATH + '/' + 'logs'
 
-DATA_URL = ['https://drive.google.com/drive/folders/1nv0QpAi9Hn9u_sZ4xy2wOZPeYaCUcGS5?usp=sharing']
+DATA_URL = ['???']
 
 DATASET_TEXT = 'Text'
 DATASET_LABEL = 'Contains_Fake_Information'
 
 DATASET_TRAIN = 'train_hate_speech_preprocessed.tsv'
 DATASET_TEST = 'test_hate_speech_preprocessed.tsv'
-
-
-# DOMAIN_GRID_SEARCH = 'gridsearch'
-# DOMAIN_TRAIN = 'training'
-# DOMAIN_VALIDATION = 'validation'
-# DOMAIN_TRAIN_ALL_DATA = 'all_data_training'
 DOMAIN_CROSS_VALIDATION = 'cross_validation'
 DOMAIN_TEST = 'test'
 
@@ -36,10 +35,3 @@ TRAIN_WORKERS = 1
 VAL_WORKERS = 1
 TEST_WORKERS = 1 
 LOGS_PATH = REPO_PATH + '/' + 'logs'
-
-# TRANSFORMERS = ['aubmindlab/araelectra-base-discriminator',
-#                 'aubmindlab/bert-base-arabertv02-twitter',
-#                 'xlm-roberta-base',
-#                 'bert-base-multilingual-cased', 
-#                 'aubmindlab/aragpt2-base',
-#                 'asafaya/albert-large-arabic']
