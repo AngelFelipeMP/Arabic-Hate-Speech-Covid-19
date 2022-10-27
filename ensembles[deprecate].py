482aece917aef370126c3015cb938d52c9318215
AngelFelipeMP <angel.magnossao@gmail.com>
2022-10-27 16:14:26 +0200

    update class Ensambles to aggregate best epoca models too

--- ensembles[deprecate].py
+++ ensembles[deprecate].py
@@ -8,26 +8,25 @@ warnings.filterwarnings('ignore')
 class Ensemble:
     def __init__(self, domain):
         self.domain = domain
-        self.file_preds = config.LOGS_PATH + '/' + self.domain + '_predictions' +'.tsv'
-        self.file_metrics = config.LOGS_PATH + '/' + self.domain + '.tsv'
-        self.ensembles = ['Majority_vote', 'Highest_sum']
-
-    def create(self):
-        ''''Run function'''
-        self.df_preds = pd.read_csv(self.file_preds, sep='\t')
+        self.file_preds = config.LOGS_PATH + '/' + domain + '_predictions' +'.tsv'
+        self.file_metrics = config.LOGS_PATH + '/' + domain + '.tsv'
         
-        if self.df_preds.columns[-1].startswith('Highest_sum') or self.df_preds.columns[-1].startswith('Majority_vote'):
+    
+    def create(self):
+        # df_metrics = pd.read_csv(self.file_metrics, sep='\t')
+        df_preds = pd.read_csv(self.file_preds, sep='\t')
+        
+        # if any(ensemble in df_metrics['transformer'].values for ensemble in ['Highest_sum', 'Majority_vote']):
+        if df_preds.columns[-1].startswith('Highest_sum') or df_preds.columns[-1].startswith('Majority_vote'):
             print('Ensemble already created')
         else:
-            self.epoch_ensembles()
-            self.best_models()
-            self.best_ensembles()
+            self.col_ensembles()
             if 'test' not in self.domain:
                 self.results_ensemble()
             print('Ensemble created')
     
-    def epoch_ensembles(self):
-        '''create ensemble for each epoch'''
+    def col_ensembles(self):
+        self.df_preds = pd.read_csv(self.file_preds, sep='\t')
         
         for epoch in range(1, config.EPOCHS+1):
             # create ensemble
@@ -38,47 +37,16 @@ class Ensemble:
             
         # save ensemble preds
         self.df_preds.to_csv(self.file_preds, index=False, sep='\t')
-    
-    def best_models(self):
-        ''''create a df with metrics of the best models in the validation data'''
-        df_cross_validation = pd.read_csv(config.LOGS_PATH + '/' + 'cross_validation.tsv', sep='\t')
-        df_cross_validation = df_cross_validation.sort_values('f1-score_val', ascending=False)
-        df_best_models = df_cross_validation.drop_duplicates(subset=['transformer'], keep='first')
-    
-        # grab bast model/epoch list
-        tuple_list = list(zip(df_best_models['epoch'], df_best_models['transformer']))
-        self.list_best_models = [(epo, trans.split('/')[-1]) for epo, trans in tuple_list if trans not in self.ensembles]
-        
-        # save tsv best models
-        df_best_models.to_csv(config.LOGS_PATH + '/' + 'cross_validation_best_models.tsv', index=False, sep='\t')
-
-    def get_pred_best_models(self):
-        ''''extract predictions (from the best models)'''
-        columns =  [col for col in self.df_preds.columns for epoch, trans in self.list_best_models if trans in col and col.endswith(str(epoch))]
-        self.best_columns = [*self.df_preds.columns[:2], *columns]
-        self.df_ensembles = self.df_preds.loc[:,self.best_columns]
-        
-    def best_ensembles(self):
-        ''''create ensembles with the predictions of the best models'''
-        self.df_preds = pd.read_csv(self.file_preds, sep='\t')
-        self.get_pred_best_models()
-        
-        self.df_preds['Majority_vote'] = self.df_ensembles.loc[:, self.best_columns[2:]].apply(lambda x: 1 if sum(self.get_vote(x)) > len(x)/2 else 0, axis=1)
-        self.df_preds['Highest_sum'] = self.df_ensembles.loc[:, self.best_columns[2:]].apply(lambda x: 1 if x.sum()/len(x) > 0.5 else 0, axis=1)
-            
-        # save ensemble preds
-        self.df_preds.to_csv(self.file_preds, index=False, sep='\t')
         
     def get_vote(self,scores):
         return [1 if sc > 0.5 else 0 for sc in scores]
+        
     
     def calculate(self, ensemble, epoch):
-        ''''calculate metrics for ensembles'''
         f1_val, acc_val, recall_val, prec_val= [], [], [], []
         
         for fold in range(1, config.SPLITS+1):
-            ensemble_col = ensemble if epoch == 'best' else ensemble + '_' + str(epoch)
-            pred_val = self.df_preds.loc[ self.df_preds['fold']==fold, [ensemble_col]].values
+            pred_val = self.df_preds.loc[ self.df_preds['fold']==fold, [ensemble + '_' + str(epoch)]].values
             targ_val = self.df_preds.loc[ self.df_preds['fold']==fold, ['target']].values
             
             f1_val.append(metrics.f1_score(targ_val, pred_val, average='binary'))
@@ -90,10 +58,9 @@ class Ensemble:
 
     
     def results_ensemble(self):
-        '''add ensemble metrics to metric table'''
         df_metrics = pd.read_csv(self.file_metrics, sep='\t')
         
-        for epoch in [l for l in range(1, config.EPOCHS+1)] + ['best']:
+        for epoch in range(1, config.EPOCHS+1):
             for ensemble in ['Majority_vote', 'Highest_sum']:
             
                 f1_val, acc_val, prec_val, recall_val = self.calculate(ensemble, epoch)
@@ -118,8 +85,8 @@ class Ensemble:
                 df_metrics = pd.concat([df_metrics, df_new_row], ignore_index=True)
             
         df_metrics.to_csv(self.file_metrics, index=False, sep='\t')
-
-
+        
+        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--test", default=False, help="Must be True or False", action='store_true')
@@ -135,10 +102,3 @@ if __name__ == "__main__":
 
     ensembles = Ensemble(domain)
     ensembles.create()
-    
-    
-    
-            #TODO:
-        # -create a table fo the paper draft 
-        # -save final predictions
-        # - change the name of the ensembles script
